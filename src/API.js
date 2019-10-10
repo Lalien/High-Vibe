c9fbfb255b849b1dfffca31420a046f0dbcd0db9
Miles Coleman <miles.lcoleman@gmail.com>
2019-10-09 21:56:34 -0600

    Changing the API to HTTPS

--- src/API.js
+++ src/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://strainapi.evanbusse.com/pfRyeU6/',
+    baseURL: 'https://strainapi.evanbusse.com/pfRyeU6/',
     responseType: 'json'
 });
